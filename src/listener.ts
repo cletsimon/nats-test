3b0213690bc9ea3d69b5eea4c91ae8b7f7b0c3c6
Clet Simon Sevillejo <cssevillejo@gmail.com>
2020-05-31 18:46:06 +0800

    even redelivery using setDeliverAllAvailable()

--- src/listener.ts
+++ src/listener.ts
@@ -19,12 +19,11 @@ stan.on('connect', () => {
     process.exit();
   });
 
-  const options = stan.subscriptionOptions().setManualAckMode(true);
-  const subscription = stan.subscribe(
-    'ticket:created',
-    'oders-service-queue-group',
-    options
-  );
+  const options = stan
+    .subscriptionOptions()
+    .setManualAckMode(true)
+    .setDeliverAllAvailable();
+  const subscription = stan.subscribe('ticket:created', options);
 
   subscription.on('message', (msg: Message) => {
     const data = msg.getData();
