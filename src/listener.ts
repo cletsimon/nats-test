bd6b15a877fef873cb93d3ccebc83d117a8a49fd
Clet Simon Sevillejo <cssevillejo@gmail.com>
2020-05-31 17:14:28 +0800

    generate random clientID

---
+++ src/listener.ts
@@ -0,0 +1,26 @@
+import nats, { Message } from 'node-nats-streaming';
+import { randomBytes } from 'crypto';
+
+console.clear();
+
+// generate random client ID
+// to run multiple listeners
+const clientID = randomBytes(4).toString('hex');
+
+const stan = nats.connect('ticketing', clientID, {
+  url: 'http://localhost:4222',
+});
+
+stan.on('connect', () => {
+  console.log('Listener connected to NATS');
+
+  const subscription = stan.subscribe('ticket:created');
+
+  subscription.on('message', (msg: Message) => {
+    const data = msg.getData();
+
+    if (typeof data === 'string') {
+      console.log(`Received event #${msg.getSequence()}, with data: ${data}`);
+    }
+  });
+});
